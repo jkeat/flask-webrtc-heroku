bbeb2c631a4357c621ae37bf60e0cf8d7f54714e
Jack Keating <keatjm@gmail.com>
2015-10-23 17:45:59 -0500

    cloud peerjs

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    peer_key = os.environ['PEERKEY']
+    return render_template("home.html", peerkey=peer_key)
 
 if __name__ == "__main__":
     app.run()
